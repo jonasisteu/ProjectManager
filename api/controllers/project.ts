09d039897454eb4fd498b0eb913f50b18aaa80fb
Jonasi STEU <jonasisteu@gmail.com>
2024-12-12 15:16:13 +0100

    [ADD] -> Project routes, controllers and models

--- api/controllers/project.ts
+++ api/controllers/project.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
+import { createProject, deleteProject, getAllProjects, getProject, updateProject } from '../models/project';
 
 export const projectConDisplayAll = async (res: Response) => {
     try {
-        const projects = await getAllProject();
+        const projects = await getAllProjects();
         res.status(200).send(projects);
     } catch {
         res.status(500).send('{"message": "Code 500: Internal Server Error}');
@@ -22,7 +23,7 @@ export const projectConSingle = async (req: Request, res: Response) => {
 export const projectConCreate = async (req: Request, res: Response) => {
     try {    
         const name = req.params.name;
-        const project = await createProject(name);
+        const project = await createProject(req);
         res.status(201).send(project);
     } catch {
         res.status(500).send('{"message": "Code 500: Internal Server Error}');
@@ -32,7 +33,7 @@ export const projectConCreate = async (req: Request, res: Response) => {
 export const projectConUpdate = async (req: Request, res: Response) => {
     try {    
         const name = req.params.name;
-        const project = await updateProject(name);
+        const project = await updateProject(req,name);
         res.status(201).send(project);
     } catch {
         res.status(500).send('{"message": "Code 500: Internal Server Error}');
--- api/models/category.ts
+++ api/models/category.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request } from 'express';
 import { Category } from "@prisma/client";
 import { prisma } from "../routes/category";
 
